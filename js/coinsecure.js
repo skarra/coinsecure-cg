18fdd2fd7f2aff8f3b905604edd2182a66081ff8
Sriram Karra <karra.etc@gmail.com>
2015-07-17 22:53:44 +0530

    Refactor and reuse form field saving code

--- js/coinsecure.js
+++ js/coinsecure.js
@@ -7,6 +7,10 @@
 // Licensed under the GNU AGPL v3
 // 
 
+var url_base = "https://api.coinsecureis.cool/v0";
+var url_high_bid = "/noauth/highestbid";
+var cur_page;
+
 function addDatePickerHandler () {
     $("#from").datepicker({
 	defaultDate: "+1w",
@@ -33,16 +37,42 @@ function addDatePickerHandler () {
 
 function saveAllFormFields () {
     console.log('Saving all form fields to localstorage...');
-    localStorage.setItem("cs_apikey", $("#apikey").val());
-    localStorage.setItem("cs_from", $("#from").val());
-    localStorage.setItem("cs_to", $("#to").val());
+
+    if (cur_page == 'transactions') {
+	localStorage.setItem("cs_apikey", $("#apikey").val());
+    }
+
+    if (cur_page == 'cg-actual') {
+	localStorage.setItem("cs_apikey", $("#apikey").val());
+	localStorage.setItem("cs_from", $("#from").val());
+	localStorage.setItem("cs_to", $("#to").val());
+    }
+
+    if (cur_page == 'cg-proj') {
+	localStorage.setItem("cs_apikey", $("#apikey").val());
+	localStorage.setItem("cs_sell_qty", $("#sell_qty").val());
+	localStorage.setItem("cs_sell_price", $("#sell_price").val());
+    }
+
     console.log('Saving all form fields to localstorage...done.');
 }
 
 function loadFormFields () {
-    $("#apikey").val(localStorage.getItem("cs_apikey"));
-    $("#from").val(localStorage.getItem("cs_from"));
-    $("#to").val(localStorage.getItem("cs_to"));
+    if (cur_page == 'transactions') {
+	$("#apikey").val(localStorage.getItem("cs_apikey"));
+    }
+
+    if (cur_page == 'cg-actual') {
+	$("#apikey").val(localStorage.getItem("cs_apikey"));
+	$("#from").val(localStorage.getItem("cs_from"));
+	$("#to").val(localStorage.getItem("cs_to"));
+    }
+
+    if (cur_page == 'cg-proj') {
+	$("#apikey").val(localStorage.getItem("cs_apikey"));
+	$("#sell_qty").val(localStorage.getItem("cs_sell_qty"));
+	$("#sell_price").val(localStorage.getItem("cs_sell_price"));
+    }
 }
 
 // Register callbacks to handle specific events on our main UI.
@@ -52,6 +82,10 @@ function addFormHandlers () {
     addDatePickerHandler();
 
     $("#compute_cg_act").submit(function() {
+	saveAllFormFields();
+    });
+
+    $("#compute_cg_proj").submit(function() {
 	saveAllFormFields();
     });
 
@@ -70,13 +104,19 @@ function setActiveNav () {
 
     if (pathname == '/') {
 	$("#nav_brand").addClass("active-nav");
+	cur_page = 'home';
     } else if (pathname == "/transactions") {
 	$("#nav_txns").addClass("active-nav");
+	cur_page = 'transactions';
     } else if (pathname == "/cgActual") {
 	$("#nav_cg_act").addClass("active-nav");
+	cur_page = 'cg-actual';
     } else if (pathname == "/cgProj") {
 	$("#nav_cg_pr").addClass("active-nav");
+	cur_page = 'cg-proj';
     }
+
+    console.log('Set current page: ' + cur_page);
 }
 
 function onLoad () {
